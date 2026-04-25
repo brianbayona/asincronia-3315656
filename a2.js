a5711971debdb5cc3deb6278fed4efb2ad51cb93
andrejoser39-dot <andrejoser39@gmail.com>
2026-04-25 10:26:16 -0500

    empezamos un conteo por milisegundos de cuando se tiene que mostrar el mensaje

---
+++ a2.js
@@ -0,0 +1,5 @@
+console.log ("1.inicio del programa"); //instrucción para empezar el programa
+
+setTimeout(() => { //setTimeout es una función que permite ejecutar un bloque de código después de un cierto tiempo (en milisegundos)
+    console.log("3. Empieza el conteo (debería haber salido antes)"); //instrucción que se ejecutará después de 100 milisegundos
+}, 100); //100 milisegundos es el tiempo que se espera antes de ejecutar la función dentro del setTimeout
