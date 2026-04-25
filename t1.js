18bb28a1a2c64228cc3bed578e9916e61fa19a65
andrejoser39-dot <andrejoser39@gmail.com>
2026-04-25 12:05:14 -0500

    comentamos con mensajes especificos sobre las funciones

--- t1.js
+++ t1.js
@@ -4,13 +4,13 @@ const colaDeSoporte = [ /// Simulamos una cola de soporte técnico con tiempos d
     { nombre: "Carlos", tiempo: 3000 },
     { nombre: "Beatriz", tiempo: 1000 }
 ];
-const atenderSolicitud = (usuario) => {
-    return new Promise((resolve) => {
-        console.log(`[Inicio] Atendiendo a ${usuario.nombre}...`);
+const atenderSolicitud = (usuario) => {// Función que simula la atención de una solicitud de soporte técnico
+    return new Promise((resolve) => { // Retorna una promesa que se resuelve después de un tiempo determinado
+        console.log(`[Inicio] Atendiendo a ${usuario.nombre}...`);  // Imprime el inicio de la atención    
         
-        setTimeout(() => {
-            console.log(`[Fin] ${usuario.nombre} atendido en ${usuario.tiempo}ms.`);
+        setTimeout(() => {  // Simula el tiempo de atención con setTimeout
+            console.log(`[Fin] ${usuario.nombre} atendido en ${usuario.tiempo}ms.`); // Imprime el fin de la atención con el tiempo que tomó
             resolve(usuario.tiempo); // Retorna el tiempo que tomó
-        }, usuario.tiempo);
+        }, usuario.tiempo); // El tiempo de atención se determina por el campo "tiempo" del usuario
     });
 };
